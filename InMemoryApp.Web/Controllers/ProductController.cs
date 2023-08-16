4b0dcfad931aab1272c2fb85dab48078a2cbdefe
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-08-16 14:25:53 +0300

    Add project files.

---
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Web.Controllers;
+
+public class ProductController : Controller
+{
+    private IMemoryCache _memoryCache;
+    public ProductController(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
+    public IActionResult SetCache()
+    {
+        MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+
+        options.AbsoluteExpiration = DateTime.Now.AddSeconds(10);
+        //options.AbsoluteExpiration = DateTime.Now.AddMicroseconds(1);
+        //options.SlidingExpiration = TimeSpan.FromSeconds(10);
+
+        options.Priority = CacheItemPriority.Normal;
+
+        options.RegisterPostEvictionCallback((key, value, reason, state) =>
+        {
+            _memoryCache.Set("callback", $"{key}-->{value}=>reason:{reason}");
+        });
+        
+        _memoryCache.Set<string>("date", DateTime.Now.ToString(), options);
+
+        return View();
+    }
+    public IActionResult GetCache()
+    {
+        #region Remove
+        //_memoryCache.Remove("date"); 
+        #endregion
+
+        #region GetOrCreate
+        //_memoryCache.GetOrCreate<string>("date", entry =>
+        //{
+        //    entry.AbsoluteExpiration = DateTime.Now.AddSeconds(30);
+        //    entry.SlidingExpiration = TimeSpan.FromSeconds(5);
+        //    return DateTime.Now.ToString();
+        //}); 
+        #endregion
+
+        _memoryCache.TryGetValue("date", out string date);
+        _memoryCache.TryGetValue<string>("callback", out string callback);
+        ViewBag.date = date;
+        ViewBag.callback = callback;
+        return View();
+    }
+}
